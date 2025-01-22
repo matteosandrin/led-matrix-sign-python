280dd7493a075198fdd9e4135320040621c0c3ae
Matteo Sandrin <matteo.sandrin@hotmail.it>
2025-01-22 16:12:05 -0500

    Organize colors and fonts into classes

--- display.py
+++ display.py
@@ -17,6 +17,19 @@ SCREEN_WIDTH = PANEL_WIDTH * PANEL_COUNT
 SCREEN_HEIGHT = PANEL_HEIGHT
 CURRENT_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
+class Fonts:
+    MBTA = ImageFont.truetype(os.path.join(
+        CURRENT_FOLDER, "fonts/MBTASans-Regular.otf"), 8)
+    SILKSCREEN = ImageFont.truetype(os.path.join(
+        CURRENT_FOLDER, "fonts/Silkscreen-Normal.ttf"), 8)
+    PICOPIXEL = ImageFont.truetype(os.path.join(
+        CURRENT_FOLDER, "fonts/Picopixel.ttf"), 7)
+
+class Colors:
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    MBTA_AMBER = (255, 191, 0)
+    SPOTIFY_GREEN = (29, 185, 84)
 
 class Display:
     def __init__(self):
@@ -32,26 +45,12 @@ class Display:
 
         self.matrix = RGBMatrix(options=options)
         self.canvas = self.matrix.CreateFrameCanvas()
-        self._load_fonts()
 
-        self.color_black = (0, 0, 0)
-        self.color_mbta_amber = (255, 191, 0)
-        self.color_spotify_green = (29, 185, 84)
+        self.default_font = Fonts.SILKSCREEN
 
     def _update_display(self, image: Image):
         self.canvas.SetImage(image)
         self.canvas = self.matrix.SwapOnVSync(self.canvas)
-    
-    def _load_fonts(self):
-        self.fonts = {
-            "mbta": ImageFont.truetype(os.path.join(
-                CURRENT_FOLDER, "fonts/MBTASans-Regular.otf"), 8),
-            "silkscreen": ImageFont.truetype(os.path.join(
-                CURRENT_FOLDER, "fonts/Silkscreen-Normal.ttf"), 8),
-            "picopixel": ImageFont.truetype(os.path.join(
-                CURRENT_FOLDER, "fonts/Picopixel.ttf"), 7),
-        }
-        self.default_font = self.fonts["silkscreen"]
 
     def _get_draw_context_antialiased(self, image: Image):
         draw = ImageDraw.Draw(image)
@@ -61,13 +60,13 @@ class Display:
     def render_text_content(self, text: str):
         image = Image.new('RGB', (SCREEN_WIDTH, SCREEN_HEIGHT))
         draw = self._get_draw_context_antialiased(image)
-        draw.text((0, 0), text, font=self.default_font, fill=(255, 255, 255))
+        draw.text((0, 0), text, font=self.default_font, fill=Colors.WHITE)
         self._update_display(image)
 
     def render_mbta_content(self, content: Tuple[PredictionStatus, List[Prediction]]):
         # Create new image with black background
         image = Image.new(
-            'RGB', (SCREEN_WIDTH, SCREEN_HEIGHT), self.color_black)
+            'RGB', (SCREEN_WIDTH, SCREEN_HEIGHT), Colors.BLACK)
         draw = self._get_draw_context_antialiased(image)
         status, predictions = content
 
@@ -82,22 +81,22 @@ class Display:
             p1, p2 = predictions[0], predictions[1]
 
             # Draw first prediction line
-            draw.text((0, 0), p1.label, font=self.fonts["mbta"], fill=self.color_mbta_amber)
-            value_width = draw.textlength(p1.value, font=self.fonts["mbta"])
+            draw.text((0, 0), p1.label, font=Fonts.MBTA, fill=Colors.MBTA_AMBER)
+            value_width = draw.textlength(p1.value, font=Fonts.MBTA)
             x_pos = max(PANEL_WIDTH * 3, SCREEN_WIDTH - value_width)
-            draw.text((x_pos, 0), p1.value, font=self.fonts["mbta"],
-                      fill=self.color_mbta_amber)
+            draw.text((x_pos, 0), p1.value, font=Fonts.MBTA,
+                      fill=Colors.MBTA_AMBER)
 
             # Draw second prediction line
-            draw.text((0, 16), p2.label, font=self.fonts["mbta"], fill=self.color_mbta_amber)
-            value_width = draw.textlength(p2.value, font=self.fonts["mbta"])
+            draw.text((0, 16), p2.label, font=Fonts.MBTA, fill=Colors.MBTA_AMBER)
+            value_width = draw.textlength(p2.value, font=Fonts.MBTA)
             x_pos = max(PANEL_WIDTH * 3, SCREEN_WIDTH - value_width)
             draw.text((x_pos, 16), p2.value,
-                      font=self.fonts["mbta"], fill=self.color_mbta_amber)
+                      font=Fonts.MBTA, fill=Colors.MBTA_AMBER)
 
             # Draw cached data indicator if needed
             if status == PredictionStatus.ERROR_SHOW_CACHED:
-                draw.point((SCREEN_WIDTH - 1, 0), fill=self.color_mbta_amber)
+                draw.point((SCREEN_WIDTH - 1, 0), fill=Colors.MBTA_AMBER)
 
         elif status in [PredictionStatus.OK_SHOW_ARR_BANNER_SLOT_1,
                         PredictionStatus.OK_SHOW_ARR_BANNER_SLOT_2,
@@ -106,16 +105,16 @@ class Display:
             return
         else:
             draw.text((0, 0), "Failed to fetch MBTA data",
-                      font=self.default_font, fill=self.color_mbta_amber)
+                      font=self.default_font, fill=Colors.MBTA_AMBER)
 
         self._update_display(image)
 
     def render_mbta_banner_content(self, status: PredictionStatus, predictions: List[Prediction]):
         # Create new image with black background
         image = Image.new(
-            'RGB', (SCREEN_WIDTH, SCREEN_HEIGHT), self.color_black)
+            'RGB', (SCREEN_WIDTH, SCREEN_HEIGHT), Colors.BLACK)
         draw = self._get_draw_context_antialiased(image)
-        mbta_font = self.fonts["mbta"]
+        mbta_font = Fonts.MBTA
 
         if status in [PredictionStatus.OK_SHOW_ARR_BANNER_SLOT_1,
                       PredictionStatus.OK_SHOW_ARR_BANNER_SLOT_2]:
@@ -132,12 +131,12 @@ class Display:
             x1 = (SCREEN_WIDTH - width1) // 2
             x2 = (SCREEN_WIDTH - width2) // 2
 
-            draw.text((x1, 0), line1, font=mbta_font, fill=self.color_mbta_amber)
-            draw.text((x2, 16), line2, font=mbta_font, fill=self.color_mbta_amber)
+            draw.text((x1, 0), line1, font=mbta_font, fill=Colors.MBTA_AMBER)
+            draw.text((x2, 16), line2, font=mbta_font, fill=Colors.MBTA_AMBER)
 
         elif status == PredictionStatus.OK_SHOW_STATION_BANNER:
             draw.text((0, 0), predictions[0].label,
-                      font=self.default_font, fill=self.color_mbta_amber)
+                      font=self.default_font, fill=Colors.MBTA_AMBER)
 
         self._update_display(image)
 
@@ -145,7 +144,7 @@ class Display:
         status, song = content
 
         # Create new image with black background
-        image = Image.new('RGB', (SCREEN_WIDTH, SCREEN_HEIGHT), self.color_black)
+        image = Image.new('RGB', (SCREEN_WIDTH, SCREEN_HEIGHT), Colors.BLACK)
         draw = self._get_draw_context_antialiased(image)
 
         if status in [SpotifyResponse.OK, SpotifyResponse.OK_SHOW_CACHED]:
@@ -164,7 +163,7 @@ class Display:
             if current_bar_width > 0:
                 draw.rectangle(
                     [(32, SCREEN_HEIGHT - 2), (32 + current_bar_width, SCREEN_HEIGHT)],
-                    fill=self.color_spotify_green  # Spotify green
+                    fill=Colors.SPOTIFY_GREEN
                 )
             
             # Draw time progress
@@ -172,7 +171,7 @@ class Display:
             time_to_end = self._format_time((song.duration_ms - song.progress_ms) // 1000, True)
             
             # Use smaller font for time display
-            small_font = self.fonts["picopixel"]
+            small_font = Fonts.PICOPIXEL
 
             # Draw song title and artist
             draw.text((32+1, 0), song.title, font=small_font, fill=(255, 255, 255))
@@ -180,12 +179,12 @@ class Display:
             # Draw progress time (left side)
             progress_time_y = SCREEN_HEIGHT - 8
             draw.text((32 + 1, progress_time_y), progress_time, 
-                    font=small_font, fill=self.color_spotify_green)
+                    font=small_font, fill=Colors.SPOTIFY_GREEN)
             
             # Draw time to end (right side)
             time_to_end_width = draw.textlength(time_to_end, font=small_font)
             draw.text((SCREEN_WIDTH - time_to_end_width - 1, progress_time_y), 
-                    time_to_end, font=small_font, fill=self.color_spotify_green)
+                    time_to_end, font=small_font, fill=Colors.SPOTIFY_GREEN)
             
             # Draw album art if available
             if song.cover.data is not None:
@@ -197,11 +196,11 @@ class Display:
             
         elif status == SpotifyResponse.EMPTY:
             draw.text((0, 0), "Nothing is playing", 
-                    font=self.default_font, fill=self.color_spotify_green)
+                    font=self.default_font, fill=Colors.SPOTIFY_GREEN)
             self._update_display(image)
         else:
             draw.text((0, 0), "Error querying the spotify API", 
-                    font=self.default_font, fill=self.color_spotify_green)
+                    font=self.default_font, fill=Colors.SPOTIFY_GREEN)
             self._update_display(image)
 
     def _format_time(self, seconds: int, is_negative: bool) -> str:
