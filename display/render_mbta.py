611b0392acd46a1b0da73dcd866aa74ef175043d
Matteo Sandrin <matteo.sandrin@hotmail.it>
2025-02-24 09:21:17 -0500

    Simplify mbta drawing code

--- display/render_mbta.py
+++ display/render_mbta.py
@@ -16,30 +16,16 @@ def render_mbta_content(
     if status in [mbta.PredictionStatus.OK,
                   mbta.PredictionStatus.ERROR_SHOW_CACHED,
                   mbta.PredictionStatus.ERROR_EMPTY]:
-
         # Swap predictions if first line is empty
         if not predictions[0].label:
             predictions[0], predictions[1] = predictions[1], predictions[0]
 
-        p1, p2 = predictions[0], predictions[1]
+        for i, p in enumerate(predictions):
+            draw.text((0, i * 16), p.label, font=Fonts.MBTA,
+                    fill=Colors.MBTA_AMBER)
+            draw.text((display.SCREEN_WIDTH, i * 16), p.value, font=Fonts.MBTA,
+                    fill=Colors.MBTA_AMBER, anchor="rt")
 
-        # Draw first prediction line
-        draw.text((0, 0), p1.label, font=Fonts.MBTA,
-                  fill=Colors.MBTA_AMBER)
-        value_width = draw.textlength(p1.value, font=Fonts.MBTA)
-        x_pos = max(display.PANEL_WIDTH * 3, display.SCREEN_WIDTH - value_width)
-        draw.text((x_pos, 0), p1.value, font=Fonts.MBTA,
-                  fill=Colors.MBTA_AMBER)
-
-        # Draw second prediction line
-        draw.text((0, 16), p2.label, font=Fonts.MBTA,
-                  fill=Colors.MBTA_AMBER)
-        value_width = draw.textlength(p2.value, font=Fonts.MBTA)
-        x_pos = max(display.PANEL_WIDTH * 3, display.SCREEN_WIDTH - value_width)
-        draw.text((x_pos, 16), p2.value,
-                  font=Fonts.MBTA, fill=Colors.MBTA_AMBER)
-
-        # Draw cached data indicator if needed
         if status == mbta.PredictionStatus.ERROR_SHOW_CACHED:
             draw.point((display.SCREEN_WIDTH - 1, 0), fill=Colors.MBTA_AMBER)
     else:
@@ -52,7 +38,6 @@ def render_mbta_content(
 
 def render_mbta_banner_content(display, lines: [str]):
     lines = lines[:2]
-    # Create new image with black background
     animations = {
         "mbta_banner": MBTABannerAnimation(
             Rect(0, 32, display.SCREEN_WIDTH, display.SCREEN_HEIGHT),
