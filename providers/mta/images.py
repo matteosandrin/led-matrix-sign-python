f0ebe9442d85fd4e356ea4bf02815734932fc0fe
Matteo Sandrin <matteo.sandrin@hotmail.it>
2025-03-02 16:37:07 -0500

    Add C train image

--- providers/mta/images.py
+++ providers/mta/images.py
@@ -40,10 +40,10 @@ route_images: Dict[str, Dict] = {
         "img" : Image.open(image_dir / 'mta_A.png'),
         "color" : "#0039A6"
     },
-    # "C" : {
-    #     "img" : Image.open(image_dir / 'mta_C.png'),
-    #     "color" : "#0039A6"
-    # },
+    "C" : {
+        "img" : Image.open(image_dir / 'mta_C.png'),
+        "color" : "#0039A6"
+    },
     # "E" : {
     #     "img" : Image.open(image_dir / 'mta_E.png'),
     #     "color" : "#0039A6"
