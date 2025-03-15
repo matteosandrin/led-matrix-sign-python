289e2e761f289b8c7dd464f93d11ecaec807352e
Matteo Sandrin <matteo.sandrin@hotmail.it>
2025-03-14 21:43:47 -0400

    Show error when importing

--- config/__init__.py
+++ config/__init__.py
@@ -1,6 +1,7 @@
 try:
     from .config import *
-except ImportError:
+except ImportError as e:
     print("ERROR: Config file not found. Please create a config/config.py file.")
     print("       Start by copying config/config.example.py to config/config.py")
+    print(e)
     exit(1)
